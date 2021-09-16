5a0368a3637fbb1bf21cc370767d8b1c6c49535c
btcn <btcn@noemail.com>
2021-09-16 15:19:12 -0400

    Fix fractional power and negative cashflow issue
    - fix fractional powers of negative numbers issue
    (explanation https://stackoverflow.com/a/45384691)
    
    - improve  initial IRR estimate

--- beangrow/returns.py
+++ beangrow/returns.py
@@ -60,7 +60,7 @@ class Pricer:
 
 def net_present_value(irr: float, cash_flows: Array, years: Array) -> float:
     """Net present value; objective function for optimizer."""
-    return np.sum(cash_flows / (1. + irr) ** years)
+    return np.sum(cash_flows / (np.sign(1. + irr) * (np.abs(1. + irr)) ** years))
 
 
 def compute_irr(dated_flows: List[CashFlow],
@@ -83,7 +83,7 @@ def compute_irr(dated_flows: List[CashFlow],
     years = np.array(years)
 
     # Start with something reasonably normal.
-    estimated_irr = 0.20
+    estimated_irr = 0.2 * np.sign(np.sum(cash_flows))
 
     # Solve for the root of the NPV equation.
     irr, unused_infodict, unused_ier, unused_mesg = fsolve(
